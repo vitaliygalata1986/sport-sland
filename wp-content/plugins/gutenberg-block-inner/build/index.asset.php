7b6555a51a58de684c2414196412f7a8b75e72a6
vitaliy <nertis44@gmail.com>
2025-03-31 00:13:50 +0300

    added atyles

--- wp-content/plugins/gutenberg-block-inner/build/index.asset.php
+++ wp-content/plugins/gutenberg-block-inner/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd2d5bec93417c95f496d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd4931b7e26a5b4eca812');
--- wp-content/plugins/gutenberg-block-inner/src/edit.js
+++ wp-content/plugins/gutenberg-block-inner/src/edit.js
@@ -1,10 +1,52 @@
-import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	InnerBlocks,
+	InspectorControls,
+} from '@wordpress/block-editor';
+import { PanelBody, RangeControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 import './editor.scss';
 
-export default function Edit() {
+export default function Edit({ attributes, setAttributes }) {
+	const { columns } = attributes;
 	return (
-		<div {...useBlockProps()}>
-			<InnerBlocks allowedBlocks={['vitos/myblock']} />
+		<div
+			{...useBlockProps({
+				className: `the-${columns}-columns`,
+			})}
+		>
+			<InspectorControls>
+				<PanelBody>
+					<RangeControl
+						label={__('Columns', 'myblocks')}
+						min={1}
+						max={4}
+						value={columns}
+						onChange={(val) => setAttributes({ columns: val })}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<InnerBlocks
+			/*
+        template={[
+          [
+            'vitos/myblock',
+            {
+              title: 'Smth title',
+              description: 'Smth description',
+            },
+          ],
+          [
+            'vitos/myblock',
+            {
+              title: 'Smth title 2',
+              description: 'Smth description 2',
+            },
+          ],
+        ]}
+        allowedBlocks={['vitos/myblock']}
+		*/
+			/>
 		</div>
 	);
 }
